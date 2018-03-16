81e7bf5b94f1fd4f0a707f95940640300e5b10f0
Yusuke Tsutsumi <yusuket@zillowgroup.com>
2018-03-16 14:06:18 -0700

    using published orbital-core, setting up travis build.

--- ubuild.py
+++ ubuild.py
@@ -4,9 +4,16 @@ current_build.config.set_defaults({
     "module": "event_tracking"
 })
 
-current_build.packages.install("../orbital-core", develop=True)
+current_build.packages.install("orbital-core")
 from orbital_core.build import bootstrap_build
 bootstrap_build(current_build)
+
+@build.task
+def build_statics(build):
+    build.executables.run(["npm", "install", "."])
+    build.executables.run(["gulp", "build"])
+
+build.tasks.append("main", "build_statics")
 
 @build.task
 def start_db(build):
@@ -20,5 +27,5 @@ def stop_db(build):
     build.executables.run(["/bin/bash", "-c", "docker stop tycho-db"])
     build.executables.run(["/bin/bash", "-c", "docker rm tycho-db"])
 
-# build.tasks.prepend("test", "start_mongodb_docker")
-# build.tasks.append("test", "stop_mongodb_docker")
+build.tasks.prepend("test", "start_db")
+build.tasks.append("test", "stop_db")
